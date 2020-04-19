4074d064405bf54a465a66c88c15ef5cc270775c
Saurabh Dhingra <Saurabh Dhingra@192.168.1.9>
2020-04-19 22:23:35 +0530

    UnOptitimsed Base Test class

--- ModularFramework-12042020/src/test/java/in/co/mercuryTravel/tests/BaseTest.java
+++ ModularFramework-12042020/src/test/java/in/co/mercuryTravel/tests/BaseTest.java
@@ -3,11 +3,22 @@ package in.co.mercuryTravel.tests;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 import commonLibs.implementation.CommonDriver;
+import commonLibs.implementation.ScreenshotControl;
 import commonLibs.utils.ConfigFileUtils;
+import commonLibs.utils.DateUtils;
 import in.co.mercuryTravel.pages.HomePage;
 
 public class BaseTest {
@@ -27,14 +38,25 @@ public class BaseTest {
 	static Properties configProperties;
 
 	static String currentWorkingDirectory;
+	static String executionStartDate;
 
 	int pageloadtimeout;
 	int elementDetectionTimeout;
 
+	ExtentHtmlReporter htmlReporter;
+	ExtentReports extent;
+	ExtentTest extentTest;
+
+	String reportFilename;
+
+	String screenshotFilename;
+	ScreenshotControl screenshotControl;
+
 	static {
 		try {
 			currentWorkingDirectory = System.getProperty("user.dir");
-			
+			executionStartDate = DateUtils.getCurrentDateAndTime();
+
 			configFileName = String.format("%s/config/config.properties", currentWorkingDirectory);
 
 			configProperties = ConfigFileUtils.readProperties(configFileName);
@@ -44,35 +66,89 @@ public class BaseTest {
 
 	}
 
+	@BeforeSuite
+	public void preSetup() {
+
+		reportFilename = String.format("%s/reports/MercuryTravelTestReport-%s.html", currentWorkingDirectory,
+				executionStartDate);
+
+		htmlReporter = new ExtentHtmlReporter(reportFilename);
+
+		extent = new ExtentReports();
+
+		extent.attachReporter(htmlReporter);
+
+	}
+
 	@BeforeClass
-	public void invokeBrowser() throws Exception {
+	public void setup() throws Exception {
+
+		extentTest = extent.createTest("Setup - Set up the pre-requisit to run automated test cases");
 
 		browserType = configProperties.getProperty("browserType");
+
+		extentTest.log(Status.INFO, "Browser invoked is " + browserType);
 
 		cmnDriver = new CommonDriver(browserType);
 
 		pageloadtimeout = Integer.parseInt(configProperties.getProperty("pageloadTimeout"));
 
+		extentTest.log(Status.INFO, "Page load Timeout set is - " + pageloadtimeout);
+
 		elementDetectionTimeout = Integer.parseInt(configProperties.getProperty("elementDetectionTimeout"));
+
+		extentTest.log(Status.INFO, "Implicit Wait set is - " + elementDetectionTimeout);
 
 		cmnDriver.setPageloadTimeout(pageloadtimeout);
 		cmnDriver.setElementDetectionTimeout(elementDetectionTimeout);
 
 		baseUrl = configProperties.getProperty("baseUrl");
+
+		extentTest.log(Status.INFO, "Base URL where the browser navigates to - " + baseUrl);
 		cmnDriver.navigateToFirstUrl(baseUrl);
 
 		driver = cmnDriver.getDriver();
 
+		extentTest.log(Status.INFO, "Initializing all pages ");
 		homepage = new HomePage(driver);
 
+		screenshotControl = new ScreenshotControl(driver);
 		homepage.closeInitialModal();
 
 	}
 
 	@AfterClass
-	public void closeBrowser() throws Exception {
+	public void cleanUp() throws Exception {
 
 		cmnDriver.closeAllBrowsers();
+
+		extentTest = extent.createTest("Clean Up - Clean process");
+		extentTest.log(Status.INFO, "Closing all browser instances" + baseUrl);
+	}
+
+	@AfterSuite
+	public void postCleanUp() {
+		extent.flush();
+	}
+
+	@AfterMethod
+	public void afterMethod(ITestResult result) throws Exception {
+		String testcaseName = result.getName();
+
+		String screenshotFilename = String.format("%s/screenshot/%s-%s.jpeg", currentWorkingDirectory, testcaseName,
+				executionStartDate);
+
+		if (result.getStatus() == ITestResult.SUCCESS) {
+			extentTest.log(Status.PASS, "Test case pass - " + testcaseName);
+		} else if (result.getStatus() == ITestResult.FAILURE) {
+			extentTest.log(Status.FAIL, "Test case fail - " + testcaseName);
+
+			screenshotControl.captureAndSaveScreenshot(screenshotFilename);
+
+			extentTest.addScreenCaptureFromPath(screenshotFilename);
+		} else {
+			extentTest.log(Status.SKIP, "Test case skipped - " + testcaseName);
+		}
 
 	}
 }
--- ModularFramework-12042020/src/test/java/in/co/mercuryTravel/tests/MercuryTravelTestcases.java
+++ ModularFramework-12042020/src/test/java/in/co/mercuryTravel/tests/MercuryTravelTestcases.java
@@ -3,22 +3,30 @@ package in.co.mercuryTravel.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
 public class MercuryTravelTestcases extends BaseTest {
 
 	@Test
 	public void verifyUserLoginWithCorrectCredentials() throws Exception {
+		
+		extentTest = extent.createTest("TC-001 - Verify UserLogin with correct credentials");
 
 		String username = configProperties.getProperty("userEmailId");
+		extentTest.log(Status.INFO, "User Email Id - " + username);
 		String password = configProperties.getProperty("userPassword");
-
+		extentTest.log(Status.INFO, "User Password - " + password);
+		
 		homepage.userLogin(username, password);
+		
+		
 
 		String expectedWelcomeText = configProperties.getProperty("expectedWelcomeText");
 
 		String actualWelcomeText = homepage.getWelcomeMessage();
 
 		Assert.assertEquals(actualWelcomeText, expectedWelcomeText);
-
+		extentTest.log(Status.INFO, "User Login Successful" );
 	}
 
 }
